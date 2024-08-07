dd1392b7e776cd68e3aeba9fae8696aa9c22454f
Gulnaz Abdiashim <abdiashimgulnaz06@mail.ru>
2024-08-08 02:05:26 +0600

    short reverse longer

--- index.js
+++ index.js
@@ -11,4 +11,13 @@ function between(a, b) {
   function sixToast(num) {
     return (num >= 6) ? num - 6 : num;
   }
+  
+  
+  function shorter_reverse_longer(a, b) {
+
+    return (a.length >= b.length) ?
+      `${b}${a.split('').reverse().join('')}${b}`
+      :
+      `${a}${b.split('').reverse().join('')}${a}`
+  }
 
