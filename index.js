6554dc8b8e7d31f84edf094143db0b9a27b2fc73
Gulnaz Abdiashim <159135738+abdiashim06@users.noreply.github.com>
2024-08-09 19:57:00 +0600

    Update index.js

--- index.js
+++ index.js
@@ -76,3 +76,113 @@ var replaceDots = function(str) {
     :`Oi! Sheep number ${ans}! You are about to be eaten by a wolf!`
     
   }
+
+
+var summation = function (num) {
+  let arr = []
+
+    while(num >= 1){
+      arr.push(num)
+      num--
+    }
+
+  return arr.reduce((a,b)=> a+b)
+    
+}
+
+
+function  calculateAge(birth,year) {
+
+
+if(birth-year === 1){
+      return `You will be born in 1 year.`
+  } else if(birth-year >1){
+      return `You will be born in ${Math.abs(year-birth)} years.`
+  } else if(year-birth === 1){
+      return `You are 1 year old.`
+  } else if(year-birth > 1){
+      return `You are ${year-birth} years old.`
+  } else if(year-birth === 0){
+      return`You were born this very year!`
+  }
+
+}
+
+
+function whatday(num) { 
+  switch(num) {
+    case 1: return "Sunday";
+    case 2: return "Monday";
+    case 3: return "Tuesday";
+    case 4: return "Wednesday";
+    case 5: return "Thursday";
+    case 6: return "Friday";
+    case 7: return "Saturday";
+    default: return 'Wrong, please enter a number between 1 and 7';
+  }
+}
+
+
+function finalGrade (exam, proj) {
+
+  return (exam > 90 || proj > 10) ? 100 
+    : (exam > 75 && proj >= 5) ? 90 
+    : (exam > 50 && proj >= 2) ? 75
+    : 0
+
+}
+
+
+function stringClean(s){
+  return s.replace(/\d+/g, "")
+}
+
+
+function isOpposite(s1,s2){
+
+if(!s1.length || s1.length !== s2.length){
+  return false
+}else{
+  
+  let lo = s1.split('').map(a => a == a.toLowerCase())
+  let up = s2.split('').map(a => a == a.toUpperCase())
+
+  return lo.every((a,i)=> a === up[i])
+
+
+}
+  
+function unusualFive() {
+  return ['a','a','a','a','a'].length
+}
+unusualFive()
+
+
+function getRealFloor(n) {
+
+  if(n<=0){
+    return n
+  } else if(n<13){
+    return n-1
+  } else if(n>13){
+    return n-2
+  }
+
+}
+
+getRealFloor(1)
+
+
+
+function powersOfTwo(n){
+  var result = [];
+  for (var i = 0; i <= n; i++) {
+    result.push(Math.pow(2, i));
+  }
+  return result;
+}
+
+
+function solution(a, b){
+  return (b.length > a.length) ? `${a}${b}${a}` : `${b}${a}${b}`
+}
